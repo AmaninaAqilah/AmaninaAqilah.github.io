4484394dba66af0c6afe1c809d997d85a12d0bc2
Nur Amanina <20ftt1456@student.pb.edu.bn>
2022-04-15 21:21:30 +0800

    Dist folder created

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <div class="bg-white">
   <div class="max-w-2xl mx-auto py-24 px-4 grid items-center grid-cols-1 gap-y-16 gap-x-8 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
    <div>
-      <img src="src/assets/image.jpg" class="bg-gray-100 rounded-lg">
+      <img src="./assets/image.jpg" class="bg-gray-100 rounded-lg">
     </div>
     <div>
       <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Nur Amanina Aqilah</h2>
