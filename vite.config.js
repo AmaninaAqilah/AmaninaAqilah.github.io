5dec3b4b2741a40e68f9d090437c604efd444dda
Nur Amanina <20ftt1456@student.pb.edu.bn>
2022-04-16 08:21:39 +0800

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: NurrAmanina.github.io,
+  // base: AmaninaAqilah.github.io,
   plugins: [vue()]
 })
